700cdf1238f0b9066492ea906c345c44bb9dff21
Herko <herko@index.hu>
2021-03-16 16:46:53 +0100

    DigitsCounter Done

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,6 +1,24 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
 
+    public int getCountOfDigits(String s) {
+        Set<Character> digits = new HashSet<>();
+        if (s == null) {
+            return 0;
+        }
+
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if(Character.isDigit(c)){
+                digits.add(c);
+            }
+        }
+        return digits.size();
+    }
 
 }
